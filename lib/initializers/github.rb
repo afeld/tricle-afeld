a8be983d53dc6c4b7d70e5634dcce92175046775
Aidan Feldman <aidan.feldman@gmail.com>
2013-08-19 05:27:46 -0400

    use pagination built into octokit

--- lib/initializers/github.rb
+++ lib/initializers/github.rb
@@ -2,31 +2,21 @@ require 'octokit'
 require 'tricle/range_data'
 
 token = ENV['GITHUB_TOKEN'] || raise("please set GITHUB_TOKEN - https://github.com/settings/applications")
-client = Octokit::Client.new(login: 'me', oauth_token: token)
 
 GITHUB_COMMITS_BY_TIME = Tricle::RangeData.new
 
-hit_three_months_ago = false
-oldest_time = Time.now.beginning_of_week.weeks_ago(13).freeze
+# GitHub restricts this API to 300 events, so the historical numbers may be low
+# http://developer.github.com/v3/activity/events/
+client = Octokit::Client.new(auto_traversal: true, login: 'me', oauth_token: token)
+events = client.user_public_events('afeld')
 
-(1..30).each do |page|
-  events = client.user_public_events('afeld', page: page)
-
-  events.each do |event|
-    time = Time.parse(event['created_at']).to_time
-    if event['type'] == 'PushEvent'
-      event['payload']['commits'].each do |commit|
-        GITHUB_COMMITS_BY_TIME.add(time, commit)
-      end
-    end
-
-    if time < oldest_time
-      hit_three_months_ago = true
-      break
+events.each do |event|
+  time = Time.parse(event['created_at']).to_time
+  if event['type'] == 'PushEvent'
+    event['payload']['commits'].each do |commit|
+      GITHUB_COMMITS_BY_TIME.add(time, commit)
     end
   end
-
-  break if hit_three_months_ago
 end
 
 GITHUB_COMMITS_BY_TIME.freeze
