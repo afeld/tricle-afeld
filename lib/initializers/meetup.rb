47b7bc59974456856e6c9ef0a854c17d7487cb47
Aidan Feldman <aidan.feldman@gmail.com>
2013-09-05 01:47:53 +0100

    let faraday handle parameter encoding

--- lib/initializers/meetup.rb
+++ lib/initializers/meetup.rb
@@ -10,9 +10,21 @@ MEETUPS_BY_START_TIME = Tricle::RangeData.new
 #
 # original URL
 # https://api.meetup.com/2/events?&sign=true&status=past&group_urlname=hackerhours&time=-3m,&key=KEY
-url = 'http://api.meetup.com/2/events?status=past&order=time&limited_events=False&group_urlname=hackerhours&desc=false&offset=0&format=json&page=200&fields=&time=-3m%2C&sig_id=8818215&sig=df406624ed3e11fa4652094ea3bca75bb5c4fe74'
+response = Faraday.get('http://api.meetup.com/2/events',
+  status: 'past',
+  order: 'time',
+  limited_events: 'False',
+  group_urlname: 'hackerhours',
+  desc: false,
+  offset: 0,
+  format: 'json',
+  page: 200,
+  fields: '',
+  time: '-3m,',
+  sig_id: 8818215,
+  sig: 'df406624ed3e11fa4652094ea3bca75bb5c4fe74'
+)
 
-response = Faraday.get(url)
 data = JSON.parse(response.body)
 data['results'].each do |event|
   start_at = Time.at(event['time'] / 1000.0)
