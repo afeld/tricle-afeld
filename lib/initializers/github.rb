11430f3fd618cb998e7ffab1c64517281328fa7a
Aidan Feldman <afeld@github.com>
2014-04-10 14:20:01 -0700

    update gems

--- lib/initializers/github.rb
+++ lib/initializers/github.rb
@@ -7,12 +7,12 @@ GITHUB_COMMITS_BY_TIME = Tricle::RangeData.new
 
 # GitHub restricts this API to 300 events, so the historical numbers may be low
 # http://developer.github.com/v3/activity/events/
-client = Octokit::Client.new(auto_traversal: true, login: 'me', oauth_token: token)
+client = Octokit::Client.new(auto_traversal: true, login: 'me', access_token: token)
 events = client.user_public_events('afeld')
 
 events.each do |event|
-  time = Time.parse(event['created_at']).to_time
   if event['type'] == 'PushEvent'
+    time = event['created_at']
     event['payload']['commits'].each do |commit|
       GITHUB_COMMITS_BY_TIME.add(time, commit)
     end
