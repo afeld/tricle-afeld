4f8e3e8b34a66ade66a497abff555a8f96b9acbd
Aidan Feldman <aidan.feldman@gmail.com>
2014-10-08 01:50:36 -0400

    simplify summing of RSVPs

--- lib/metrics/hacker_hours_rsvps.rb
+++ lib/metrics/hacker_hours_rsvps.rb
@@ -5,11 +5,11 @@ class HackerHoursRsvps < Tricle::Metric
 
   def size_for_range(start_at, end_at)
     meetups = MEETUPS_BY_START_TIME.items_for_range(start_at, end_at)
-    meetups.reduce(0){|sum, event| sum + event['yes_rsvp_count'] }
+    meetups.sum {|event| event['yes_rsvp_count'] }
   end
 
   def total
     meetups = MEETUPS_BY_START_TIME.all_items
-    meetups.reduce(0){|sum, event| sum + event['yes_rsvp_count'] }
+    meetups.sum {|event| event['yes_rsvp_count'] }
   end
 end
